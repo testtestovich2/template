406a90bc75e09bd0017773417922f8e22331a31b
Alexey Anasov <webdev.alexey94@yandex.by>
2022-06-19 22:26:08 +0300

    adaptive + pixel perfect

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    480: {
+      sliderPerView: 1,
+    }
+  }
+});
